92830bd54bcbcab6c8ad21909b43502722bfeaf2
Johan Groenen <johan.groenen@gmail.com>
2022-06-12 23:38:59 +0200

    work on presi

--- js/Presi.mjs
+++ js/Presi.mjs
@@ -1,8 +1,27 @@
-export default function (cbase) {
-    function test () {
-        console.log(cbase);
+export default function (cbase, template) {
+    var showcases = cbase["_embedded"];
+
+    function arrayRotate(arr, reverse) {
+        if (reverse) arr.unshift(arr.pop());
+        else arr.push(arr.shift());
+        return arr;
     }
+
+    function start() {
+        document.getElementById("showcase").innerHTML = tmpl("showcase_template", showcases[0]);
+    }
+
+    function next() {
+        document.getElementById("showcase").innerHTML = tmpl("showcase_template", arrayRotate(showcases)[0]);
+    }
+
+    function prev() {
+        document.getElementById("showcase").innerHTML = tmpl("showcase_template", arrayRotate(showcases, true)[0]);
+    }
+
     return {
-        test: test
+        start: start,
+        next: next,
+        prev: prev
     };
 };
